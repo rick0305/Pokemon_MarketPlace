3a8e90762fc75bd1f4962bfbcb8262daa68138e1
Diego Cravo Teixeira <dcravote@everis.com>
2021-08-03 14:33:36 -0300

    search by type

--- src/pages/PokemonType/index.js
+++ src/pages/PokemonType/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TypeMenu from '../../components/TypeMenu';
 import TypeRender from "../../components/TypeRender"
+import Footer from "../../components/Footer"
 
 function Type() {
   return (
       <div style={{marginTop: "70px"}}>
           {/* <TypeMenu /> */}
           <TypeRender />
+          {/* <Footer /> */}
       </div>
   );
 }
--- src/redux/pokemonType.js
+++ src/redux/pokemonType.js
@@ -1,46 +1,96 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState: {
     value: 1,
   },
   reducers: {
     setNormal: (state) => {
-        state.value = 1
+      state.value = 1;
     },
     setfighting: (state) => {
-        state.value = 2
+      state.value = 2;
     },
     setflying: (state) => {
-        state.value = 3
+      state.value = 3;
     },
     setpoison: (state) => {
-        state.value = 4
+      state.value = 4;
     },
     setground: (state) => {
-        state.value = 5
+      state.value = 5;
     },
     setrock: (state) => {
-        state.value = 6
+      state.value = 6;
     },
     setbug: (state) => {
-        state.value = 7
+      state.value = 7;
     },
     setghost: (state) => {
-        state.value = 8
+      state.value = 8;
     },
     setsteel: (state) => {
-        state.value = 9
+      state.value = 9;
     },
     setfire: (state) => {
-        state.value = 10
+      state.value = 10;
     },
-
+    setwater: (state) => {
+      state.value = 11;
+    },
+    setgrass: (state) => {
+      state.value = 12;
+    },
+    setelectric: (state) => {
+      state.value = 13;
+    },
+    setpsychic: (state) => {
+      state.value = 14;
+    },
+    setice: (state) => {
+      state.value = 15;
+    },
+    setdragon: (state) => {
+      state.value = 16;
+    },
+    setdark: (state) => {
+      state.value = 17;
+    },
+    setfairy: (state) => {
+      state.value = 18;
+    },
+    setunknown: (state) => {
+      state.value = 19;
+    },
+    setshadow: (state) => {
+      state.value = 20;
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { setNormal, setfighting, setflying, setpoison, setground, setrock, setbug, setghost, setsteel, setfire } = counterSlice.actions
+export const {
+  setNormal,
+  setfighting,
+  setflying,
+  setpoison,
+  setground,
+  setrock,
+  setbug,
+  setghost,
+  setsteel,
+  setfire,
+  setwater,
+  setgrass,
+  setelectric,
+  setpsychic,
+  setice,
+  setdragon,
+  setdark,
+  setfairy,
+  setunknown,
+  setshadow,
+} = counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
