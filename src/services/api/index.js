6796abdff7e9f04c123bf259edab32fa9cc957ca
Diego Cravo Teixeira <dcravote@everis.com>
2021-08-04 14:53:07 -0300

    json server update

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001'
+  // baseURL: 'http://localhost:3001'
+  baseURL: 'https://pokefeira.herokuapp.com'
 })
 
 //localhost padrao: 3001
